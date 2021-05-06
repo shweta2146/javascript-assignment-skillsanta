587dd914c10f3f5cb18209f94da8895f99acc5a6
Shweta Mahajan <shweta@iotric.com>
2021-05-07 00:59:31 +0530

    third commit

---
+++ 4_integer.js
@@ -0,0 +1,9 @@
+let a = parseInt(prompt("Enter any integer"))
+if(a>=50 && a<=100){
+    document.write(`${a} is in range of 50-100`);
+
+}
+else{
+    document.write(`${a} is in not range of 50-100`);
+
+}
