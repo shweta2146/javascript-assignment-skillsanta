c9ac2ab92ff6c52b992f948d19e37d1256e1de83
Shweta Mahajan <shweta@iotric.com>
2021-05-07 00:50:55 +0530

    second commit

---
+++ 8_for_loop.js
@@ -0,0 +1,10 @@
+for(var i = 20; i<=30; i++){
+    if(i%2 ==0){
+    document.write(`${i} is even`);
+    }
+    else {
+    document.write(`${i} is odd`);
+        
+    }
+    document.write("<br>");
+}
